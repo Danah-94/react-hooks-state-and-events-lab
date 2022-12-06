26357eb3b50d1166f630c2dc16d47db1c53d6529
Danah <osta.danah@gmail.com>
2022-12-07 01:09:40 +0200

    Completed Assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+   
+   const [add, setAdd] = useState(false);
+
+   function AddToCart() {
+       setAdd( add => !add)
+   }
+
+   const AddOrNot = add ? "in-cart" : "" ;
+   const textColor = add? "white" : "black" ;
+   const backgroundColor = add?  "rgb(210, 51, 210)" : "rgb(237, 237, 32)" ;
+  
   return (
-    <li className="">
+    <li className={AddOrNot}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={AddToCart} className="add" style={{ color: textColor, backgroundColor: backgroundColor }}>{ add ? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
